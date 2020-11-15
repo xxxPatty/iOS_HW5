e85f823bee463f701f8460cca64de46947dc29a4
xxxPatty <L224749391@gmail.com>
2020-11-15 20:23:05 +0800

    Initial Commit

---
+++ iOS_HW5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_HW5
+//
+//  Created by 林湘羚 on 2020/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_HW5/iOS_HW5App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_HW5App.swift
+//  iOS_HW5
+//
+//  Created by 林湘羚 on 2020/11/15.
+//
+
+import SwiftUI
+
+@main
+struct iOS_HW5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
